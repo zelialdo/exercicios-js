a2cccb989278dea66701a87aa408ee9d9dee17fa
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-03-20 09:48:54 -0300

    mais exercicios

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,17 @@
+// Amarzenando uma funcao em uma variavel
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2 , 3)
+
+// armazenando um funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// retorno implicito
+const subtracao = (a, b) => a-b
+console.log(subtracao(2, 3))
